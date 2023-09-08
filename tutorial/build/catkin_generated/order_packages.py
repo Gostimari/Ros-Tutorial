e70fde4bdc14687b8f0a4660ab04e6ef40c6a1f0
Gostimari <duarte-cruz-98@outlook.com>
2023-09-08 18:54:47 +0100

    Cleanup extra files

--- tutorial/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/duarte/tutorial/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/duarte/tutorial/devel;/home/duarte/ros-robot/devel;/opt/ros/noetic'.split(';') if '/home/duarte/tutorial/devel;/home/duarte/ros-robot/devel;/opt/ros/noetic' != '' else []
--- tutorial/build/stage_tutorial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/duarte/tutorial/src/stage_tutorial
-# Build directory: /home/duarte/tutorial/build/stage_tutorial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
